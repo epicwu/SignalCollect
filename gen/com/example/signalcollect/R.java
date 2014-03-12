ba855a08ca21854cc808c7bb8dab59cdc48609c3
Jun Wu <wuj4@ws-45.cs.hut.fi>
2014-03-12 13:02:38 +0200

    collect ss & wifi

--- gen/com/example/signalcollect/R.java
+++ gen/com/example/signalcollect/R.java
@@ -24,7 +24,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f08000b;
+        public static final int linearLayout1=0x7f080000;
+        public static final int textView1=0x7f080007;
+        public static final int textView2=0x7f080003;
+        public static final int textView3=0x7f080005;
+        public static final int textView4=0x7f080001;
+        public static final int textView5=0x7f080009;
+        public static final int tvTick=0x7f080004;
+        public static final int txViewDebug=0x7f080008;
+        public static final int txViewGPS=0x7f080006;
+        public static final int txViewModel=0x7f080002;
+        public static final int wifiInfo=0x7f08000a;
     }
     public static final class layout {
         public static final int activity_signal_collect=0x7f030000;
@@ -35,7 +46,13 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int cellphoneModel=0x7f050008;
+        public static final int debugInfo=0x7f050003;
+        public static final int gpsInfo=0x7f050004;
         public static final int hello_world=0x7f050002;
+        public static final int initialService=0x7f050006;
+        public static final int noGPS=0x7f050005;
+        public static final int runningTime=0x7f050007;
     }
     public static final class style {
         /** 
--- src/com/example/signalcollect/SignalCollect.java
+++ src/com/example/signalcollect/SignalCollect.java
@@ -1,22 +1,175 @@
 package com.example.signalcollect;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import java.text.SimpleDateFormat;
 
+
+import android.location.LocationManager;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
+import android.os.StrictMode;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.TelephonyManager;
+import android.telephony.gsm.GsmCellLocation;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.TextView;
+
+@SuppressLint("NewApi")
 public class SignalCollect extends Activity {
 
+	//power management
+	PowerManager powerManager = null;
+	WakeLock wakeLock = null;
+	
+	boolean stateDebug = true;
+	private static final String TAG = "State_Info";  
+	static BackgroundCollecting backCollect = null;
+	
+	// global variables
+	int runningTime = 0;
+	double gpsLat=0,gpsLon=0;
+	
+	// Location manager
+	private LocationManager locManager = null;   
+	//Tele manager
+	TelephonyManager teleManager = null;
+	//WiFi manager
+	WifiManager wifiManager = null;
+	
+	//textViews
+	TextView tvTick = null; 
+	TextView tvGPS = null;
+	TextView tvModel = null;
+	TextView tvDebug = null;
+	TextView tvWiFi = null;
+	
+
+	
+	//handler, receiving latest info from background service and display it
+	private Handler myHandler = new Handler(){  
+	          
+	        public void handleMessage(Message msg) {
+	        	if (msg.what == 1)
+	        	{
+	        		Bundle bundle = msg.getData();
+	        		runningTime =Integer.parseInt((String) bundle.get("tick"));
+	        		if (bundle.getString("lat")!=null)
+	        		gpsLat = Double.parseDouble(bundle.getString("lat"));
+	        		if (bundle.getString("lon")!=null)
+	        		gpsLon = Double.parseDouble(bundle.getString("lon"));
+	        		if (bundle.getString("neighbor")!=null)
+	        			tvDebug.setText(bundle.getString("neighbor"));
+	        		Log.e("Counter", Integer.toString(runningTime));
+	        		
+	        		if (tvWiFi != null)
+	        		{
+	        			tvWiFi.setText((String)bundle.get("wifi"));
+	        		}
+	        	}
+	        	SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");    
+	        	String date=sdf.format(new java.util.Date()); 
+	        	tvTick.setText(Integer.toString(runningTime)+" "+date);
+	        	tvGPS.setText("lat:" + gpsLat + "\nlon:" + gpsLon);
+	        };  
+	};  
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_signal_collect);
+		
+		if (android.os.Build.VERSION.SDK_INT > 9) {
+		    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+		    StrictMode.setThreadPolicy(policy);
+		}
+		
+		/*
+		 * Hold the CPU
+		*/
+		powerManager = (PowerManager) getSystemService(POWER_SERVICE);
+		wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,"MyWakelockTag");
+		wakeLock.acquire();
+		
+		tvTick = (TextView) findViewById(R.id.tvTick);
+		tvGPS = (TextView) findViewById(R.id.txViewGPS);
+		tvModel = (TextView) findViewById(R.id.txViewModel);
+		
+		String MODEL_NAME = Build.MODEL;
+		MODEL_NAME = MODEL_NAME.replace(" ", "_");
+		MODEL_NAME = MODEL_NAME.replace("-", "_");
+		tvModel.setText(MODEL_NAME);
+		
+		tvDebug = (TextView) findViewById(R.id.txViewDebug);
+		tvWiFi = (TextView) findViewById(R.id.wifiInfo);
+		
+		//启动采集服务 start collecting information
+		// 获取系统LocationManager服务  
+        locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);  
+        //获取tele manager
+        teleManager = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+        wifiManager = (WifiManager)getSystemService(Context.WIFI_SERVICE);  
+        //Gsm manager
+        if (backCollect == null)
+        {
+        	backCollect = new BackgroundCollecting();
+        	backCollect.start(new Messenger(myHandler),locManager,teleManager,wifiManager,MODEL_NAME);
+        }
+        
+
+        
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.signal_collect, menu);
 		return true;
 	}
-
+	@Override
+	protected void onStart() {  
+        super.onStart();
+        
+        //------------------------
+        if (stateDebug) Log.e(TAG, "###start onStart###");
+    }  
+    @Override  
+    protected void onPause() {  
+        super.onPause();  
+        if (stateDebug) Log.e(TAG, "###start onPause###");  
+    }  
+    @Override  
+    protected void onStop() {  
+        super.onStop();  
+        if (stateDebug) Log.e(TAG, "###start onStop###");  
+    }  
+    @Override  
+    protected void onDestroy() {  
+        super.onDestroy();  
+        if (stateDebug) Log.e(TAG, "###onDestroy###");
+        //关闭采集服务
+        //close the collecting service
+        wakeLock.release();
+        if (backCollect!=null)
+        backCollect.end();
+    }  	
+    
+    @Override  
+    public void onBackPressed() {
+    	// back键不会退出，而是隐藏界面
+    	// when user press 'BACK' button, hide user interface, do not exit
+        Intent i= new Intent(Intent.ACTION_MAIN);   
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);   
+        i.addCategory(Intent.CATEGORY_HOME);   
+        startActivity(i);  
+    }
 }
+
